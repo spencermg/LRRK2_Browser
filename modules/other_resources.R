6fc00257495e72f16377a976bae565db8ec7b86f
Spencer Grant <spencermgrant3@gmail.com>
2025-10-21 17:08:45 -0400

    Add more external resources

--- modules/other_resources.R
+++ modules/other_resources.R
@@ -20,30 +20,20 @@ otherResourcesServer <- function(id) {
                     width = 3,
                     align = "center",
                     tags$a(
-                        href = "https://www.ncbi.nlm.nih.gov/gene/120892",
+                        href = "https://www.mdsgene.org/genes/PARK-LRRK2",
                         target = "_blank",
                         style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "NCBI (Gene Summary)"
+                        "MDSGene"
                     ),
                 ),
                 column(
                     width = 3,
                     align = "center",
                     tags$a(
-                        href = "https://www.uniprot.org/uniprotkb/Q5S007/entry",
+                        href = "https://www.omim.org/entry/609007",
                         target = "_blank",
                         style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "UniProt (Protein Function & Domains)"
-                    ),
-                ),
-                column(
-                    width = 3,
-                    align = "center",
-                    tags$a(
-                        href = "https://www.genecards.org/cgi-bin/carddisp.pl?gene=LRRK2",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "GeneCards (Integrative database)"
+                        "OMIM"
                     ),
                 ),
                 column(
@@ -53,9 +43,79 @@ otherResourcesServer <- function(id) {
                         href = "https://www.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000188906",
                         target = "_blank",
                         style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "Ensembl (Functional Annotations)"
+                        "Ensembl"
                     ),
-                )
+                ),
+                column(
+                    width = 3,
+                    align = "center",
+                    tags$a(
+                        href = "https://gnomad.broadinstitute.org/gene/ENSG00000188906",
+                        target = "_blank",
+                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
+                        "gnomAD"
+                    ),
+                ),
+                column(
+                    width = 3,
+                    align = "center",
+                    tags$a(
+                        href = "https://www.ncbi.nlm.nih.gov/clinvar/?term=%22LRRK2%22%5BGENE%5D&redir=gene",
+                        target = "_blank",
+                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
+                        "Clinvar"
+                    ),
+                ),
+                column(
+                    width = 3,
+                    align = "center",
+                    tags$a(
+                        href = "https://gp2.broadinstitute.org/gene/ENSG00000188906",
+                        target = "_blank",
+                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
+                        "GP2 Variant Browser"
+                    ),
+                ),
+                column(
+                    width = 3,
+                    align = "center",
+                    tags$a(
+                        href = "https://pdgenetics.shinyapps.io/GP2Browser/",
+                        target = "_blank",
+                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
+                        "GP2 GWAS Browser"
+                    ),
+                ),
+                column(
+                    width = 3,
+                    align = "center",
+                    tags$a(
+                        href = "https://www.ncbi.nlm.nih.gov/gene/120892",
+                        target = "_blank",
+                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
+                        "NCBI"
+                    ),
+                ),
+                column(
+                    width = 6,
+                    align = "center",
+                    tags$a(
+                        href = "https://www.uniprot.org/uniprotkb/Q5S007/entry",
+                        target = "_blank",
+                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
+                        "UniProt"
+                    ),
+                ),
+                column(
+                    width = 6,
+                    align = "center",
+                    tags$a(
+                        href = "https://www.genecards.org/cgi-bin/carddisp.pl?gene=LRRK2",
+                        target = "_blank",
+                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
+                        "GeneCards"
+                    ),
+                ),
             )
         })
     })
