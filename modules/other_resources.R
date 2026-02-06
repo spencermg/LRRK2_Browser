eb0f4ecf1f6a8948c089b2544ecc2163c2464c96
spencermg <spencermgrant3@gmail.com>
2026-02-06 15:12:34 -0500

    Added logo images for the external resources

--- modules/other_resources.R
+++ modules/other_resources.R
@@ -12,120 +12,102 @@ otherResourcesUI <- function(id) {
 # SERVER FUNCTION
 # =========================================================================
 
+height <- 90
+horizontal_spacing <- 30
+
 otherResourcesServer <- function(id) {
     moduleServer(id, function(input, output, session) {
         output$otherResources <- renderUI({
-            fluidRow(
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://www.ncbi.nlm.nih.gov/clinvar/?term=%22LRRK2%22%5BGENE%5D&redir=gene",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "Clinvar"
-                    ),
+            tags$div(
+                style = paste0("display: flex; flex-wrap: wrap; gap: ", horizontal_spacing, "px; justify-content: center;"),
+                tags$a(
+                    href = "https://www.ncbi.nlm.nih.gov/clinvar/?term=%22LRRK2%22%5BGENE%5D&redir=gene",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/clinvar.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://www.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000188906",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "Ensembl"
-                    ),
+                tags$a(
+                    href = "https://www.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000188906",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/ensembl.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://www.genecards.org/cgi-bin/carddisp.pl?gene=LRRK2",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "GeneCards"
-                    ),
+                tags$a(
+                    href = "https://www.genecards.org/cgi-bin/carddisp.pl?gene=LRRK2",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/genecards.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://gnomad.broadinstitute.org/gene/ENSG00000188906",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "gnomAD"
-                    ),
+                tags$a(
+                    href = "https://gnomad.broadinstitute.org/gene/ENSG00000188906",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/gnomad.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://pdgenetics.shinyapps.io/GP2Browser/",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "GP2 GWAS Browser"
-                    ),
+                tags$a(
+                    href = "https://pdgenetics.shinyapps.io/GP2Browser/",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/gwas_browser.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://gp2.broadinstitute.org/gene/ENSG00000188906",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "GP2 Variant Browser"
-                    ),
+                tags$a(
+                    href = "https://gp2.broadinstitute.org/gene/ENSG00000188906",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/gwas_browser.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://ldlink.nih.gov/ldpair",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "LD Pair"
-                    ),
+                tags$a(
+                    href = "https://ldlink.nih.gov/ldpair",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/ldlink.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://www.mdsgene.org/genes/PARK-LRRK2",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "MDSGene"
-                    ),
+                tags$a(
+                    href = "https://www.mdsgene.org/genes/PARK-LRRK2",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/mdsgene.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://www.ncbi.nlm.nih.gov/gene/120892",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "NCBI"
-                    ),
+                tags$a(
+                    href = "https://www.ncbi.nlm.nih.gov/gene/120892",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/ncbi.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://www.omim.org/entry/609007",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "OMIM"
-                    ),
+                tags$a(
+                    href = "https://www.omim.org/entry/609007",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/omim.png",
+                        style = paste0("height:", height, "px;")
+                    )
                 ),
-                column(
-                    width = 2,
-                    align = "center",
-                    tags$a(
-                        href = "https://www.uniprot.org/uniprotkb/Q5S007/entry",
-                        target = "_blank",
-                        style = "font-size:18px; font-weight:bold; color:#0C8DC3; text-decoration:none;",
-                        "UniProt"
-                    ),
-                ),
+                tags$a(
+                    href = "https://www.uniprot.org/uniprotkb/Q5S007/entry",
+                    target = "_blank",
+                    tags$img(
+                        src = "icons/uniprot.png",
+                        style = paste0("height:", height, "px;")
+                    )
+                )
             )
         })
     })
